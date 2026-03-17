44bba85d6420eac13a494060d20c1be67d765248
Shravanis30 <salunkeshravani561@gmail.com>
2026-03-17 15:00:00 +0530

    docs: update API documentation

--- src/services/fabric.service.js
+++ src/services/fabric.service.js
@@ -99,3 +99,4 @@ module.exports = new FabricService();
 
 // update on 2026-03-15 - refactor: optimize backend performance
 // update on 2026-03-16 - feat: optimize blockchain interaction
+// update on 2026-03-17 - docs: update API documentation

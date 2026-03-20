7dbb41cba98ceadaa5a865568a89880dd2c9b802
Shravanis30 <salunkeshravani561@gmail.com>
2026-03-20 14:13:00 +0530

    style: improve UI responsiveness

--- src/services/fabric.service.js
+++ src/services/fabric.service.js
@@ -101,3 +101,4 @@ module.exports = new FabricService();
 // update on 2026-03-16 - feat: optimize blockchain interaction
 // update on 2026-03-17 - docs: update API documentation
 // update on 2026-03-17 - docs: update API documentation
+// update on 2026-03-20 - style: improve UI responsiveness

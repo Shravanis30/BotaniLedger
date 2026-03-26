994be2990a2fc48558609a62439805d96a9700cb
Shravanis30 <salunkeshravani561@gmail.com>
2026-03-26 13:46:00 +0530

    feat: update dashboard UI components

--- src/services/fabric.service.js
+++ src/services/fabric.service.js
@@ -108,3 +108,4 @@ module.exports = new FabricService();
 // update on 2026-03-23 - feat: optimize blockchain interaction
 // update on 2026-03-24 - docs: update API documentation
 // update on 2026-03-24 - refactor: optimize backend performance
+// update on 2026-03-26 - feat: update dashboard UI components

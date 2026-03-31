7e49291e1bf28728b30e37dfaef627c045b68f4d
Shravanis30 <salunkeshravani561@gmail.com>
2026-03-31 10:29:00 +0530

    refactor: optimize backend performance

--- src/app.js
+++ src/app.js
@@ -64,3 +64,4 @@ module.exports = app;
 // update on 2026-03-25 - feat: improve farmer batch handling
 // update on 2026-03-26 - docs: update API documentation
 // update on 2026-03-28 - feat: optimize blockchain interaction
+// update on 2026-03-31 - refactor: optimize backend performance

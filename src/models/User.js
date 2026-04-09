1b48d9d9bc11c97c65b3732b2a5ccc4cbabe4e44
Shravanis30 <salunkeshravani561@gmail.com>
2026-04-09 14:01:00 +0530

    refactor: optimize backend performance

--- src/models/User.js
+++ src/models/User.js
@@ -78,3 +78,4 @@ module.exports = mongoose.model('User', UserSchema);
 // update on 2026-04-05 - feat: improve farmer batch handling
 // update on 2026-04-06 - feat: optimize blockchain interaction
 // update on 2026-04-08 - refactor: improve code structure
+// update on 2026-04-09 - refactor: optimize backend performance

585d7e03477fd7c380073aabac03a9f3e296c1fe
Shravanis30 <salunkeshravani561@gmail.com>
2026-04-10 10:02:00 +0530

    feat: optimize blockchain interaction

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -111,3 +111,4 @@ exports.updateProfile = async (req, res) => {
 // update on 2026-04-01 - refactor: improve code structure
 // update on 2026-04-01 - refactor: optimize backend performance
 // update on 2026-04-07 - feat: update dashboard UI components
+// update on 2026-04-10 - feat: optimize blockchain interaction
